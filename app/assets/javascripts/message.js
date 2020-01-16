4325f76f39d2b5b23acdfe06cdd0ffaeec56ec5d
tomio1030 <59253596+tomio1030@users.noreply.github.com>
2020-01-16 17:03:17 +0900

    Update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -4,7 +4,7 @@ $(function() {
    if ( message.image ) {
      var html =
       `<div class = "message" data-message-id=${message.id}>
-      <div class="upper-message" >
+        <div class="upper-message" >
          <div class="upper-info">
            <div class="upper-info__user-name">
              ${message.user_name}
